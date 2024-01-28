e3d20adbd75c3bd904083346d98d3271ba0e93ca
Igor Pak <ipak.tulane@gmail.com>
2024-01-28 21:19:10 +0500

    Add base project

---
+++ test4/test4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test4
+//
+//  Created by iPak Tulane on 28/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
